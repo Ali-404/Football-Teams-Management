238db45019af6e3310c5ea99b3b72b537eb315cc
Ali-404 <abdellali.aitaddi@gmail.com>
2024-11-17 23:28:31 +0100

    Remove CardFooter import from PlayersContainer.tsx

--- src/components/data/PlayersContainer.tsx
+++ src/components/data/PlayersContainer.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react'
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '../ui/card'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '../ui/card'
 import SearchBar from '../app/SearchBar'
 import { ScrollArea } from '../ui/scroll-area'
 import { Button } from '../ui/button'
